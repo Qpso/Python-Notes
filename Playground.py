61b58b6bce350b8cae370d57858c6776704fa868
Qpso <128926086+Qpso@users.noreply.github.com>
2023-06-03 15:12:22 +0100

    Update from Windows

--- Playground.py
+++ Playground.py
@@ -1,19 +1,8 @@
-# empty playground for tests
-# Reading a file using open() function
-file_path = "example.txt"
-try:
-    with open(file_path, "r") as file:
-        content = file.read()
-        print("File content:")
-        print(content)
-except FileNotFoundError:
-    print(f"File '{file_path}' not found.")
 
-# Writing to a file using open() function
-new_content = "This is the new content of the file."
-try:
-    with open(file_path, "w") as file:
-        file.write(new_content)
-        print("File updated successfully.")
-except FileNotFoundError:
-    print(f"File '{file_path}' not found.")
+
+l1 = ['blue' , 'red' , 'orange' , 'white']
+l2 = ['sky' , 'apple' , 'sunset' , 'clouds']
+
+fl = l1 + l2 
+for i1,i2 in zip(l1,l2):
+    print(i1,i2)
