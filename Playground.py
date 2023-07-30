7ba80939c5c0200b41dd7398f5c223da47eaa1f8
Qpso <128926086+Qpso@users.noreply.github.com>
2023-07-31 00:43:03 +0100

    Auto Update from Windows

--- Playground.py
+++ Playground.py
@@ -1,7 +1,5 @@
 # empty playground for tests
 
-def save_user(**user): #the two *'s are called kwags, key word arguments        
-    print(user["name"]) # the variable inside the square brackets is the value we'll be accessing 
 
+test
 
-save_user( name : "Haris", age: 22) 
